dff0e24db178b0092e663d4b82d53bd820fc185a
glmapper <glmapper_2018@163.com>
2024-08-28 15:09:41 +0800

    init

---
+++ src/main/resources/archetype-resources/idata-web/src/main/java/Application.java
@@ -0,0 +1,21 @@
+package ${groupId};
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@MapperScan(basePackages = "${groupId}.dao.mybatis")
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        String logPath = context.getEnvironment().getProperty("logging.file.path");
+        LOGGER.info("application has been started, you can find system log by path: {}", logPath);
+    }
+}
---
+++ src/main/resources/archetype-resources/idata-web/src/test/java/AppTest.java
@@ -0,0 +1,44 @@
+package ${groupId};
+
+import ${groupId}.dao.jpa.UserRepository;
+import ${groupId}.dao.mybatis.UserMapper;
+import ${groupId}.model.entity.UserEntity;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.platform.commons.function.Try;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
+@RunWith(SpringRunner.class)
+public class TestsBase {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserMapper userMapper;
+
+    @Before
+    public void after() {
+        this.userRepository.deleteAll();
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName("glmapper");
+        userEntity.setEmail("123@qq.com");
+        this.userRepository.save(userEntity);
+    }
+
+    @Test
+    public void test() {
+        List<UserEntity> glmapper = this.userMapper.findByName("glmapper");
+        Assert.assertTrue(glmapper.size() == 1);
+
+    }
+}
+
